f144708118d543c70d879c6807cbd9d21cbc6eda
Bianca Jemsten <bianca.jemsten@sapient.com>
2018-12-10 13:59:28 +0000

    beginning

---
+++ photo-rotation-ui/src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import './App.css';
+import { Route, Switch } from 'react-router'
+import { BrowserRouter as Router } from "react-router-dom";
+import Home from './Home';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <Switch>
+          <Route path={'/'} component={Home}/>
+        </Switch>
+      </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ photo-rotation-ui/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Image from './images/[0]';
+
+
+const Home = () =>{ 
+    console.log(Image)
+    return(
+        <div></div>
+    // <img src={image} alt="peacock"/>
+)}
+
+export default Home;
