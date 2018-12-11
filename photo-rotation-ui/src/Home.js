7cf3bd2038df759a2868c41a7f8727948d9491c9
Bianca Jemsten <biancajemsten@gmail.com>
2018-12-11 23:25:13 +0000

    working rotation

--- photo-rotation-ui/src/Home.js
+++ photo-rotation-ui/src/Home.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  align-items: center;
+`
 
 
 class Home extends React.Component {
@@ -12,12 +20,10 @@ class Home extends React.Component {
     rotateImage = () => {
       clearInterval(rotating);
       const rotating = setInterval(() => {
-        console.log(this.state.currentImage)
         let state = Object.assign({}, this.state);
-        console.log('currentImage', state.currentImage)
         if(state.currentImage < 17){ this.setState({currentImage:  state.currentImage+1})}
-        else(this.setState({currentImage: 0}))
-      }, 5000)
+        else(this.setState({currentImage: 1}))
+      }, 15000)
     }
 
     componentDidMount(){
@@ -28,10 +34,10 @@ class Home extends React.Component {
       console.log('in render',this.state.currentImage)
       const image = require(`./Images/${this.state.currentImage}.jpg`)
       return(
-        <div>
-          <img src={image} alt="bild"/>
+        <ImageContainer >
+          <img className="fade" src={image} alt="bild"/>
 
-        </div>
+        </ImageContainer>
 
       )}
 
--- photo-rotation-ui/src/components/App.js
+++ photo-rotation-ui/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import './App.css';
+import '../App.css';
 import { Route, Switch } from 'react-router'
 import { BrowserRouter as Router } from "react-router-dom";
 import Home from '../Home';
